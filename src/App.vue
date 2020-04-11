c287e142cfd1c0d593df80a3919bbcc2c69da9be
wachcio <wachcio@poczta.fm>
2020-04-12 01:18:41 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      v-if="true"
+      alt="Vue logo"
+      src="./assets/logo.png"
+      title="hjghj"
+    />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  mounted() {
+    const newLocal = [2, 2, 2, 4];
+    newLocal.push("4");
+    console.log(`Hello ${newLocal}`);
+    return { arr: newLocal };
+  },
+};
 </script>
 
 <style>
